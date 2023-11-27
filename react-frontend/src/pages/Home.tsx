04655a8fe038b439c950c95ef6eace471696f746
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-11-27 18:50:49 +0530

    Banner Completed

--- react-frontend/src/pages/Home.tsx
+++ react-frontend/src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
+import Banner from "../components/Banner";
 
 const Home: FC = ()=>{
     return (
-        <h1>This is the Home Page</h1>
+        <>
+        <Banner/>
+        </>
     )
 }
 
