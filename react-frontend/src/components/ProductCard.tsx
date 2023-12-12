93ef648050f449e0c29dc5271539be7d7f719e88
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-12-12 15:36:45 +0530

    made the product cards responsive

--- react-frontend/src/components/ProductCard.tsx
+++ react-frontend/src/components/ProductCard.tsx
@@ -12,13 +12,13 @@ const ProductCard: FC<productI> = ({ _id, title, price, offer, img, deleteValue 
     }
     return (
         <>
-            <div className="lg:w-96 w-2/5 p-5 box-border bg-white lg:h-[500px] h-[560px]">
+            <div className="lg:w-96 w-2/5 p-5 box-border bg-white lg:h-[500px] md:h-[560px] h-[340px]">
                 <Link to={`/products/${_id}`}>
                     <img src={`${imge}`} alt="" className="lg:w-96 w-full h-4/5 object-cover" onMouseEnter={handleIn} onMouseLeave={handleOut} />
                 </Link>
-                <p className="text-base mt-2">{title}</p>
-                <h1 className="text-lg font-semibold">Rs.{price} <del className="text-gray-500">Rs.{deleteValue}</del></h1>
-                <h1 className="text-base text-red-500 font-semibold">{offer}</h1>
+                <p className="text-sm md:text-base md:mt-2 mt-1">{title}</p>
+                <h1 className="text-sm md:text-lg font-semibold">Rs.{price} <del className="text-gray-500">Rs.{deleteValue}</del></h1>
+                <h1 className="md:text-base hidden md:block text-red-500 md:font-semibold">{offer}</h1>
             </div>
         </>
     )
